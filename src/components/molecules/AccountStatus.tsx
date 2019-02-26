5aff55545523ff7227ee6c970949bc4675bb3556
Matthias Kretschmann <m@kretschmann.io>
2019-02-26 10:26:40 -0300

    use react-popover for smart placement of account popover

--- src/components/molecules/AccountStatus.tsx
+++ src/components/molecules/AccountStatus.tsx
@@ -2,57 +2,8 @@ import React, { PureComponent } from 'react'
 import cx from 'classnames'
 // import Button from '../atoms/Button'
 import { User } from '../../context/User'
+import Popover from 'react-popover'
 import styles from './AccountStatus.module.scss'
-
-interface AccountStatusProps {
-    className?: string
-}
-
-interface AccountStatusState {
-    popoverOpen: boolean
-}
-
-export default class AccountStatus extends PureComponent<
-    AccountStatusProps,
-    AccountStatusState
-> {
-    public state = {
-        popoverOpen: false
-    }
-
-    private togglePopover() {
-        this.setState(prevState => ({
-            popoverOpen: !prevState.popoverOpen
-        }))
-    }
-
-    public render() {
-        return (
-            <div
-                className={cx(styles.status, this.props.className)}
-                onMouseEnter={() => this.togglePopover()}
-                onMouseLeave={() => this.togglePopover()}
-                onTouchStart={() => this.togglePopover()}
-            >
-                <User.Consumer>
-                    {states =>
-                        !states.isWeb3 ? (
-                            <span className={styles.statusIndicator} />
-                        ) : !states.isLogged ? (
-                            <span
-                                className={styles.statusIndicatorCloseEnough}
-                            />
-                        ) : states.isLogged ? (
-                            <span className={styles.statusIndicatorActive} />
-                        ) : null
-                    }
-                </User.Consumer>
-
-                {this.state.popoverOpen && <AccountPopover />}
-            </div>
-        )
-    }
-}
 
 const AccountPopover = () => (
     <div className={styles.popover}>
@@ -83,3 +34,68 @@ const AccountPopover = () => (
         </div> */}
     </div>
 )
+
+interface AccountStatusProps {
+    className?: string
+}
+
+interface AccountStatusState {
+    popoverIsOpen: boolean
+    preferPlace?: string
+    place?: string
+    enterExitTransitionDurationMs?: number
+}
+
+export default class AccountStatus extends PureComponent<
+    AccountStatusProps,
+    AccountStatusState
+> {
+    public state = {
+        popoverIsOpen: false
+    }
+
+    private togglePopover(toState: boolean) {
+        const popoverIsOpen =
+            typeof toState === 'boolean' ? toState : !this.state.popoverIsOpen
+        this.setState({ popoverIsOpen })
+    }
+
+    public AccountIndicator = () => (
+        <div
+            className={cx(styles.status, this.props.className)}
+            onMouseOver={() => this.togglePopover(true)}
+            onMouseOut={() => this.togglePopover(false)}
+            onTouchStart={() => this.togglePopover(true)}
+        >
+            <User.Consumer>
+                {states =>
+                    !states.isWeb3 ? (
+                        <span className={styles.statusIndicator} />
+                    ) : !states.isLogged ? (
+                        <span className={styles.statusIndicatorCloseEnough} />
+                    ) : states.isLogged ? (
+                        <span className={styles.statusIndicatorActive} />
+                    ) : null
+                }
+            </User.Consumer>
+        </div>
+    )
+
+    public render() {
+        const popoverProps = {
+            isOpen: this.state.popoverIsOpen,
+            // preferPlace: this.state.preferPlace,
+            // place: this.state.place,
+            enterExitTransitionDurationMs: 300,
+            tipSize: 0.01,
+            onOuterAction: () => this.togglePopover(false),
+            body: <AccountPopover />
+        }
+
+        return (
+            <Popover {...popoverProps}>
+                <this.AccountIndicator />
+            </Popover>
+        )
+    }
+}
