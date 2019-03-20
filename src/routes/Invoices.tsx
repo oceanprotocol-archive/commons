601a0bd993b9afa28eb3b62a857ba7ca623f3a54
Jernej Pregelj <jernej.pregelj88@gmail.com>
2019-03-20 10:52:53 +0100

    lint fixes

--- src/routes/Invoices.tsx
+++ src/routes/Invoices.tsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import Route from '../components/templates/Route'
 import { User } from '../context/User'
 import Asset from '../components/molecules/Asset'
-import { Logger } from '@oceanprotocol/squid';
+import { Logger } from '@oceanprotocol/squid'
 import styles from './Search.module.scss'
-
 
 interface InvoicesState {
     results: any[]
@@ -14,24 +13,29 @@ export default class Invoices extends Component<{}, InvoicesState> {
     public state = { results: [] }
 
     public async componentDidMount() {
-
         // this is currently my published assets
-        this.context.ocean.keeper.didRegistry.contract.getPastEvents('DIDAttributeRegistered', {
-            filter: {_owner:this.context.account},
-            fromBlock: 0,
-            toBlock: 'latest'
-        }, async (error: any, events: any) => {
-            if (error) {
-                Logger.log('error retrieving', error)
-            } else {
-                const results = []
-                for (const event of events) {
-                    const ddo = await this.context.ocean.resolveDID(`did:op:${event.returnValues._did.substring(2)}`)
-                    results.push(ddo)
+        this.context.ocean.keeper.didRegistry.contract.getPastEvents(
+            'DIDAttributeRegistered',
+            {
+                filter: { _owner: this.context.account },
+                fromBlock: 0,
+                toBlock: 'latest'
+            },
+            async (error: any, events: any) => {
+                if (error) {
+                    Logger.log('error retrieving', error)
+                } else {
+                    const results = []
+                    for (const event of events) {
+                        const ddo = await this.context.ocean.resolveDID(
+                            `did:op:${event.returnValues._did.substring(2)}`
+                        )
+                        results.push(ddo)
+                    }
+                    this.setState({ results })
                 }
-                this.setState({ results })
             }
-        })
+        )
     }
 
     public renderResults = () =>
