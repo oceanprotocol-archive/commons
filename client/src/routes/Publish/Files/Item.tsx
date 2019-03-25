4977b28f1b995479a99ed26907f7003dcead2fce
Matthias Kretschmann <m@kretschmann.io>
2019-03-25 18:29:34 +0100

    Files styling

--- client/src/routes/Publish/Files/Item.tsx
+++ client/src/routes/Publish/Files/Item.tsx
@@ -4,14 +4,19 @@ import filesize from 'filesize'
 
 const Item = ({ item, removeItem }: { item: any; removeItem: any }) => (
     <li>
-        <a href={item.url}>{item.url}</a>
+        <a href={item.url} className={styles.linkUrl}>
+            {item.url}
+        </a>
         <div className={styles.details}>
-            <span>url: {item.found ? 'confirmed' : 'unconfirmed'}</span>
+            <span>URL {item.found ? 'confirmed' : ' not confirmed'}</span>
             <span>
-                size:
-                {item.found && item.size ? filesize(item.size) : 'unknown'}
+                {item.found && item.size ? filesize(item.size) : 'unknown size'}
             </span>
-            <span>type: {item.found && item.type ? item.type : 'unknown'}</span>
+            <span>
+                {item.found && item.type
+                    ? item.type.split('/')[1]
+                    : 'unknown type'}
+            </span>
         </div>
         <button
             type="button"
