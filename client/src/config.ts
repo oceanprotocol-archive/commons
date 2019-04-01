2dba004f7a30f1f455b4ee03dc8358bee3846f99
Matthias Kretschmann <m@kretschmann.io>
2019-04-01 16:01:51 +0200

    add remote config for Nile

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,33 +1,69 @@
+//
+// commons-server connection
+//
 export const serviceScheme = 'http'
 export const serviceHost = 'localhost'
 export const servicePort = 4000
 
-export const nodeScheme = 'http'
-export const nodeHost = 'localhost'
-export const nodePort = 8545
+//
+// OCEAN REMOTE CONNECTIONS
+//
+export const nodeScheme = 'https'
+export const nodeHost = 'nile.dev-ocean.com'
+export const nodePort = 443
 
-export const aquariusScheme = 'http'
-export const aquariusHost = 'aquarius'
-export const aquariusPort = 5000
+export const aquariusScheme = 'https'
+export const aquariusHost = 'nginx-aquarius.dev-ocean.com'
+export const aquariusPort = 443
 
-export const brizoScheme = 'http'
-export const brizoHost = 'localhost'
-export const brizoPort = 8030
+export const brizoScheme = 'https'
+export const brizoHost = 'nginx-brizo.dev-ocean.com'
+export const brizoPort = 443
 
-export const parityScheme = 'http'
-export const parityHost = 'localhost'
-export const parityPort = 8545
-
-export const secretStoreScheme = 'http'
-export const secretStoreHost = 'localhost'
-export const secretStorePort = 12001
-
+export const parityScheme = 'https'
+export const parityHost = 'nile.dev-ocean.com'
+export const parityPort = 443
 export const threshold = 0
-export const password = 'node0'
-export const address = '0x00bd138abd70e2f00903268f3db08f2d25677c9e'
+export const password = 'ocean_secret'
+export const address = '0x413c9ba0a05b8a600899b41b0c62dd661e689354'
 
-export const faucetScheme = 'http'
-export const faucetHost = 'localhost'
-export const faucetPort = 3001
+export const secretStoreScheme = 'https'
+export const secretStoreHost = 'secret-store.dev-ocean.com'
+export const secretStorePort = 443
+
+export const faucetScheme = 'https'
+export const faucetHost = 'faucet.nile.dev-ocean.com'
+export const faucetPort = 443
+
+//
+// OCEAN LOCAL CONNECTIONS
+// e.g. when running with barge
+//
+// export const nodeScheme = 'http'
+// export const nodeHost = 'localhost'
+// export const nodePort = 8545
+
+// export const aquariusScheme = 'http'
+// export const aquariusHost = 'aquarius'
+// export const aquariusPort = 5000
+
+// export const brizoScheme = 'http'
+// export const brizoHost = 'localhost'
+// export const brizoPort = 8030
+
+// export const parityScheme = 'http'
+// export const parityHost = 'localhost'
+// export const parityPort = 8545
+// export const threshold = 0
+// export const password = 'node0'
+// export const address = '0x00bd138abd70e2f00903268f3db08f2d25677c9e'
+
+// export const secretStoreScheme = 'http'
+// export const secretStoreHost = 'localhost'
+// export const secretStorePort = 12001
+
+// export const faucetScheme = 'http'
+// export const faucetHost = 'localhost'
+// export const faucetPort = 3001
 
 export const verbose = true
