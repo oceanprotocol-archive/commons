519c2a887f8670e35295607f3fe27abc3feba901
Matthias Kretschmann <m@kretschmann.io>
2019-04-03 18:41:39 +0200

    refactor to use one download per file

--- client/src/routes/Details/AssetFilesDetails.tsx
+++ client/src/routes/Details/AssetFilesDetails.tsx
@@ -1,64 +1,11 @@
 import React, { PureComponent } from 'react'
-import { Logger } from '@oceanprotocol/squid'
-import filesize from 'filesize'
-import { User } from '../../context/User'
-import Button from '../../components/atoms/Button'
-import Spinner from '../../components/atoms/Spinner'
+import AssetFile from './AssetFile'
 import styles from './AssetFilesDetails.module.scss'
 
-interface AssetFilesDetailsProps {
+export default class AssetFilesDetails extends PureComponent<{
     files: any[]
     ddo: any
-}
-
-interface AssetFilesDetailsState {
-    isLoading: boolean
-    error: string
-    message: string
-}
-
-export default class AssetFilesDetails extends PureComponent<
-    AssetFilesDetailsProps,
-    AssetFilesDetailsState
-> {
-    public state = {
-        isLoading: false,
-        error: '',
-        message: 'Decrypting files, please sign with your wallet...'
-    }
-
-    private resetState = () => this.setState({ isLoading: true, error: '' })
-
-    private purchaseAsset = async (ddo: any) => {
-        this.resetState()
-
-        const { ocean } = this.context
-        const accounts = await ocean.accounts.list()
-
-        try {
-            const service = ddo.findServiceByType('Access')
-            const agreementId = await ocean.assets.order(
-                ddo.id,
-                service.serviceDefinitionId,
-                accounts[0]
-            )
-
-            const path = await ocean.assets.consume(
-                agreementId,
-                ddo.id,
-                service.serviceDefinitionId,
-                accounts[0],
-                ''
-            )
-            Logger.log('path', path)
-
-            this.setState({ isLoading: false })
-        } catch (error) {
-            Logger.log('error', error)
-            this.setState({ isLoading: false, error: error.message })
-        }
-    }
-
+}> {
     public render() {
         const { files, ddo } = this.props
 
@@ -66,41 +13,12 @@ export default class AssetFilesDetails extends PureComponent<
             <>
                 <div className={styles.files}>
                     {files.map(file => (
-                        <ul key={file.index} className={styles.file}>
-                            <li>
-                                {file.contentType &&
-                                    file.contentType.split('/')[1]}
-                            </li>
-                            <li>
-                                {file.contentLength &&
-                                    filesize(file.contentLength)}
-                            </li>
-                            {/* <li>{file.encoding}</li> */}
-                            {/* <li>{file.compression}</li> */}
-                        </ul>
+                        <AssetFile key={file.index} ddo={ddo} file={file} />
                     ))}
                 </div>
-
-                {this.state.isLoading ? (
-                    <Spinner message={this.state.message} />
-                ) : (
-                    <Button
-                        primary
-                        className={styles.buttonMain}
-                        onClick={() => this.purchaseAsset(ddo)}
-                    >
-                        Get asset files
-                    </Button>
-                )}
-
-                {this.state.error !== '' && (
-                    <div className={styles.error}>{this.state.error}</div>
-                )}
             </>
         ) : (
             <div>No files attached.</div>
         )
     }
 }
-
-AssetFilesDetails.contextType = User
