534da80ed2c8a42018a341a3d24ed888f34328e0
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-04-05 13:10:50 +0200

    Updated react env variable names

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,37 +1,37 @@
 //
 // commons-server connection
 //
-export const serviceScheme = process.env.SERVICE_SCHEME || 'http'
-export const serviceHost = process.env.SERVICE_HOST || 'localhost'
-export const servicePort = process.env.SERVICE_PORT || 4000
+export const serviceScheme = process.env.REACT_APP_SERVICE_SCHEME || 'http'
+export const serviceHost = process.env.REACT_APP_SERVICE_HOST || 'localhost'
+export const servicePort = process.env.REACT_APP_SERVICE_PORT || 4000
 
 //
 // OCEAN REMOTE CONNECTIONS
 //
-export const nodeScheme = process.env.NODE_SCHEME || 'https'
-export const nodeHost = process.env.NODE_HOST || 'nile.dev-ocean.com'
-export const nodePort = process.env.NODE_PORT || 443
+export const nodeScheme = process.env.REACT_APP_NODE_SCHEME || 'https'
+export const nodeHost = process.env.REACT_APP_NODE_HOST || 'nile.dev-ocean.com'
+export const nodePort = process.env.REACT_APP_NODE_PORT || 443
 
-export const aquariusScheme = process.env.AQUARIUS_SCHEME || 'https'
-export const aquariusHost = process.env.AQUARIUS_HOST || 'nginx-aquarius.dev-ocean.com'
-export const aquariusPort = process.env.AQUARIUS_PORT || 443
+export const aquariusScheme = process.env.REACT_APP_AQUARIUS_SCHEME || 'https'
+export const aquariusHost = process.env.REACT_APP_AQUARIUS_HOST || 'nginx-aquarius.dev-ocean.com'
+export const aquariusPort = process.env.REACT_APP_AQUARIUS_PORT || 443
 
-export const brizoScheme = process.env.BRIZO_SCHEME || 'https'
-export const brizoHost = process.env.BRIZO_HOST || 'nginx-brizo.dev-ocean.com'
-export const brizoPort = process.env.BRIZO_PORT || 443
-export const brizoAddress = process.env.BRIZO_ADDRESS || '0x376817c638d2a04f475a73af37f7b51a2862d567'
+export const brizoScheme = process.env.REACT_APP_BRIZO_SCHEME || 'https'
+export const brizoHost = process.env.REACT_APP_BRIZO_HOST || 'nginx-brizo.dev-ocean.com'
+export const brizoPort = process.env.REACT_APP_BRIZO_PORT || 443
+export const brizoAddress = process.env.REACT_APP_BRIZO_ADDRESS || '0x376817c638d2a04f475a73af37f7b51a2862d567'
 
-export const parityScheme = process.env.PARITY_SCHEME || 'https'
-export const parityHost = process.env.PARITY_HOST || 'nile.dev-ocean.com'
-export const parityPort = process.env.PARITY_PORT || 443
+export const parityScheme = process.env.REACT_APP_PARITY_SCHEME || 'https'
+export const parityHost = process.env.REACT_APP_PARITY_HOST || 'nile.dev-ocean.com'
+export const parityPort = process.env.REACT_APP_PARITY_PORT || 443
 
-export const secretStoreScheme = process.env.SECRET_STORE_SCHEME || 'https'
-export const secretStoreHost = process.env.SECRET_STORE_HOST || 'secret-store.dev-ocean.com'
-export const secretStorePort = process.env.SECRET_STORE_PORT || 443
+export const secretStoreScheme = process.env.REACT_APP_SECRET_STORE_SCHEME || 'https'
+export const secretStoreHost = process.env.REACT_APP_SECRET_STORE_HOST || 'secret-store.dev-ocean.com'
+export const secretStorePort = process.env.REACT_APP_SECRET_STORE_PORT || 443
 
-export const faucetScheme = process.env.FAUCET_SCHEME || 'https'
-export const faucetHost = process.env.FAUCET_HOST || 'faucet.nile.dev-ocean.com'
-export const faucetPort = process.env.FAUCET_PORT || 443
+export const faucetScheme = process.env.REACT_APP_FAUCET_SCHEME || 'https'
+export const faucetHost = process.env.REACT_APP_FAUCET_HOST || 'faucet.nile.dev-ocean.com'
+export const faucetPort = process.env.REACT_APP_FAUCET_PORT || 443
 
 //
 // OCEAN LOCAL CONNECTIONS
