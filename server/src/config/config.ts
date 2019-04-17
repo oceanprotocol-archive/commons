db1d215b1c541de26145fce22f001f0b186b2434
Jernej Pregelj <jernej.pregelj88@gmail.com>
2019-04-17 14:43:44 +0200

    commented local variables

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -23,6 +23,28 @@ const config = {
         secretStorePort: process.env.SECRET_STORE_PORT || 443,
         verbose: true
     }
+    /*
+    app: {
+        port: 4000,
+        nodeScheme: 'http',
+        nodeHost: 'localhost',
+        nodePort: 8545,
+        aquariusScheme: 'http',
+        aquariusHost: 'localhost',
+        aquariusPort: 5000,
+        brizoScheme: 'http',
+        brizoHost: 'localhost',
+        brizoPort: 8030,
+        brizoAddress: '0x376817c638d2a04f475a73af37f7b51a2862d567',
+        parityScheme: 'http',
+        parityHost: 'localhost',
+        parityPort: 8545,
+        secretStoreScheme: 'http',
+        secretStoreHost: 'localhost',
+        secretStorePort: 12001,
+        verbose: true
+    }
+    */
 }
 
 export default config
