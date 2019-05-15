36d61b3fc15759df40431145241cec96665be530
Jernej Pregelj <jernej.pregelj88@gmail.com>
2019-05-15 10:15:19 +0200

    add category

--- client/src/config.ts
+++ client/src/config.ts
@@ -8,6 +8,7 @@ export const servicePort = process.env.REACT_APP_SERVICE_PORT || 4000
 //
 // OCEAN REMOTE CONNECTIONS
 //
+/*
 export const nodeScheme = process.env.REACT_APP_NODE_SCHEME || 'https'
 export const nodeHost = process.env.REACT_APP_NODE_HOST || 'nile.dev-ocean.com'
 export const nodePort = process.env.REACT_APP_NODE_PORT || 443
@@ -40,37 +41,35 @@ export const faucetScheme = process.env.REACT_APP_FAUCET_SCHEME || 'https'
 export const faucetHost =
     process.env.REACT_APP_FAUCET_HOST || 'faucet.nile.dev-ocean.com'
 export const faucetPort = process.env.REACT_APP_FAUCET_PORT || 443
-
+*/
 //
 // OCEAN LOCAL CONNECTIONS
 // e.g. when running with barge
 //
-// export const nodeScheme = 'http'
-// export const nodeHost = 'localhost'
-// export const nodePort = 8545
+export const nodeScheme = 'http'
+export const nodeHost = 'localhost'
+export const nodePort = 8545
 
-// export const aquariusScheme = 'http'
-// export const aquariusHost = 'aquarius'
-// export const aquariusPort = 5000
+export const aquariusScheme = 'http'
+export const aquariusHost = 'aquarius'
+export const aquariusPort = 5000
 
-// export const brizoScheme = 'http'
-// export const brizoHost = 'localhost'
-// export const brizoPort = 8030
+export const brizoScheme = 'http'
+export const brizoHost = 'localhost'
+export const brizoPort = 8030
+export const brizoAddress = '0x00bd138abd70e2f00903268f3db08f2d25677c9e'
 
-// export const parityScheme = 'http'
-// export const parityHost = 'localhost'
-// export const parityPort = 8545
-// export const threshold = 0
-// export const password = 'node0'
-// export const address = '0x00bd138abd70e2f00903268f3db08f2d25677c9e'
+export const parityScheme = 'http'
+export const parityHost = 'localhost'
+export const parityPort = 8545
 
-// export const secretStoreScheme = 'http'
-// export const secretStoreHost = 'localhost'
-// export const secretStorePort = 12001
+export const secretStoreScheme = 'http'
+export const secretStoreHost = 'localhost'
+export const secretStorePort = 12001
 
-// export const faucetScheme = 'http'
-// export const faucetHost = 'localhost'
-// export const faucetPort = 3001
+export const faucetScheme = 'http'
+export const faucetHost = 'localhost'
+export const faucetPort = 3001
 
 export const verbose = true
 
