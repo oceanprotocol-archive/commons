ec887bebd7bb8182a503026ccb72fcc90b3859e0
Matthias Kretschmann <m@kretschmann.io>
2019-05-29 12:01:08 +0200

    show faucet version number, small refactor

---
+++ client/src/components/atoms/VersionNumbers.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render, waitForElement } from 'react-testing-library'
+import mockAxios from 'jest-mock-axios'
+import VersionNumbers from './VersionNumbers'
+
+afterEach(() => {
+    mockAxios.reset()
+})
+
+describe('VersionNumbers', () => {
+    it('renders without crashing', () => {
+        const { container } = render(<VersionNumbers />)
+        expect(container.firstChild).toBeInTheDocument()
+    })
+
+    it('returns component versions in link title, prefixed with `v`', async () => {
+        const { getByTitle } = render(<VersionNumbers />)
+        mockAxios.mockResponse({ data: { version: '6.6.6' } })
+        expect(mockAxios.get).toHaveBeenCalled()
+        await waitForElement(() => getByTitle(/v6.6.6/))
+    })
+})
