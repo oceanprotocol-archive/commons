b8113798b6226f6f386a725e146f5b5ece37d9cc
Matthias Kretschmann <m@kretschmann.io>
2019-06-18 00:05:40 +0200

    SEO component

--- client/src/routes/NotFound.test.tsx
+++ client/src/routes/NotFound.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import NotFound from './NotFound'
+import { MemoryRouter } from 'react-router'
 
 describe('NotFound', () => {
     it('renders without crashing', () => {
-        const { container } = render(<NotFound />)
+        const { container } = render(
+            <MemoryRouter>
+                <NotFound />
+            </MemoryRouter>
+        )
         expect(container.firstChild).toBeInTheDocument()
     })
 })
