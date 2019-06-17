d364a7beef5dd547b2d41f2907736481734b1c8b
Jernej Pregelj <jernej.pregelj88@gmail.com>
2019-06-17 16:27:57 +0200

    fix web3 version + use truffle-hdwallet

--- cypress/integration/consume.spec.js
+++ cypress/integration/consume.spec.js
@@ -1,19 +1,20 @@
 /// <reference types="Cypress" />
 import Web3 from 'web3'
-import PrivateKeyProvider from 'truffle-privatekey-provider'
+import HDWalletProvider from 'truffle-hdwallet-provider'
 
 context('Consume', () => {
     beforeEach(() => {
         cy.on('window:before:load', win => {
-            const provider = new PrivateKeyProvider(
-                'd631de5b7e9cf451135896c833187c8b4dc230bf47756a9a2ca4ffccc161175e',
-                'http://localhost:8545'
+            const provider = new HDWalletProvider(
+                'taxi music thumb unique chat sand crew more leg another off lamp',
+                'https://nile.dev-ocean.com'
             )
             win.web3 = new Web3(provider)
+            win.ethereum = win.web3
         })
 
         cy.visit(
-            'http://localhost:3000/asset/did:op:52f2ed716f97463e97beeb414195a075b606675874204e3da39b0c237377dbd3'
+            'http://localhost:3000/asset/did:op:48870c70dac448949634c41817c8017dc558ce59ad774002996e9811b97a36d5'
         )
         // wait for the asset details to have loaded
         cy.get('button', { timeout: 10000 }).should('have.length', 1)
