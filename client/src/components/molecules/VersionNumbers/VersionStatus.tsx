74b8bc3f13947246a698379515037cd4f9988989
Matthias Kretschmann <m@kretschmann.io>
2019-06-19 13:43:21 +0200

    add status info tooltips

--- client/src/components/molecules/VersionNumbers/VersionStatus.tsx
+++ client/src/components/molecules/VersionNumbers/VersionStatus.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import styles from './VersionStatus.module.scss'
 
+const statusInfo: { [key: string]: string } = {
+    ok: 'Shows if connection to all component endpoints can be established.',
+    network: 'Shows if all components are on the same network.',
+    contracts: 'Shows if contracts loaded by components are the same version.'
+}
+
 const VersionStatus = ({
     status
 }: {
-    status: { ok: boolean; contracts: boolean; network: boolean }
+    status: { ok: boolean; network: boolean; contracts: boolean }
 }) => {
     return (
         <div className={styles.status}>
             {Object.entries(status).map(([key, value]) => (
-                <div className={styles.element} key={key}>
+                <div
+                    className={styles.element}
+                    key={key}
+                    title={statusInfo[key]}
+                >
                     <span
                         className={
                             value === true
