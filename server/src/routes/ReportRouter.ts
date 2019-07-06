fa078c6c4c5677a3f7bc98751c5c726f377a1e00
Matthias Kretschmann <m@kretschmann.io>
2019-07-06 14:39:38 +0200

    send Slack message

--- server/src/routes/ReportRouter.ts
+++ server/src/routes/ReportRouter.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from 'express'
 import SendgridMail from '@sendgrid/mail'
+import { WebClient } from '@slack/web-api'
 
 SendgridMail.setApiKey(process.env.SENDGRID_API_KEY)
+const slack = new WebClient(process.env.SLACK_TOKEN)
 
 export class ReportRouter {
     public router: Router
@@ -10,21 +12,26 @@ export class ReportRouter {
         this.router = Router()
     }
 
-    public async sendEmail(req: Request, res: Response) {
+    public async sendMessage(req: Request, res: Response) {
         if (!req.body.msg) {
             return res.send({ status: 'error', message: 'missing message' })
         }
 
         try {
             await SendgridMail.send(req.body.msg)
+            await slack.chat.postMessage({
+                text: req.body.msg.html,
+                channel: 'C88KZQEBB' // #form-submissions
+            })
             return res.send({ status: 'success' })
         } catch (error) {
+            console.error(`${error.code} - ${error.message}`) // eslint-disable-line
             res.send(`${error.code} - ${error.message}`)
         }
     }
 
     public init() {
-        this.router.post('/', this.sendEmail)
+        this.router.post('/', this.sendMessage)
     }
 }
 
