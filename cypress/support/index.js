01fb305612a4c85190f569870cc1ef37a2c4593d
Matthias Kretschmann <m@kretschmann.io>
2019-07-10 17:21:55 +0200

    remove wallet setup from Cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,14 +1,3 @@
 /// <reference types="Cypress" />
-import Web3 from 'web3'
-import HDWalletProvider from 'truffle-hdwallet-provider'
 
-before(function() {
-    cy.on('window:before:load', win => {
-        const provider = new HDWalletProvider(
-            Cypress.env('SEEDPHRASE'),
-            Cypress.env('NODE_URI')
-        )
-        win.web3 = new Web3(provider)
-        win.ethereum = win.web3
-    })
-})
+before(function() {})
