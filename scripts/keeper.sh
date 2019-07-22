036305c3152b5d4e4808dfb0a4be5a23c0da2ce1
Matthias Kretschmann <m@kretschmann.io>
2019-07-22 12:32:22 +0200

    copy contracts from Docker

--- scripts/keeper.sh
+++ scripts/keeper.sh
@@ -5,17 +5,25 @@
 RETRY_COUNT=0
 COMMAND_STATUS=1
 
+printf '\n\e[33mWaiting for contracts to be generated...\e[0m\n'
+
 mkdir -p artifacts
 
 until [ $COMMAND_STATUS -eq 0 ] || [ $RETRY_COUNT -eq 120 ]; do
   keeper_contracts_docker_id=$(docker container ls | grep keeper-contracts | awk '{print $1}')
-  docker cp ${keeper_contracts_docker_id}:/keeper-contracts/artifacts/ready ./artifacts/
+  docker cp "${keeper_contracts_docker_id}":/keeper-contracts/artifacts/ready ./artifacts/ > /dev/null 2>&1
   COMMAND_STATUS=$?
   sleep 5
-  let RETRY_COUNT=RETRY_COUNT+1
+  (( RETRY_COUNT=RETRY_COUNT+1 ))
 done
+
+printf '\e[32mFound new contract artifacts.\e[0m\n'
+
+rm -rf ./artifacts/
 
 if [ $COMMAND_STATUS -ne 0 ]; then
   echo "Waited for more than two minutes, but keeper contracts have not been migrated yet. Did you run an Ethereum RPC client and the migration script?"
   exit 1
 fi
+
+docker cp "${keeper_contracts_docker_id}":/keeper-contracts/artifacts/. ./client/node_modules/@oceanprotocol/keeper-contracts/artifacts/
