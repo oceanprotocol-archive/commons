fc83106be27f56e2c3ecc35906153b3d0f72e10b
Santiago Gonzalez Toral <hernangt12re3@gmail.com>
2019-12-05 12:58:07 -0500

    Add react-hot-reload for op-web3-wallet in dev mode

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,25 +3,53 @@ import ReactDOM from 'react-dom'
 import UserProvider from './context/UserProvider'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { AppContainer } from 'react-hot-loader';
+require('react-hot-loader/patch')
 
-function renderToDOM() {
-    const root = document.getElementById('root')
+const root = document.getElementById('root')
 
-    if (root !== null) {
+ReactDOM.render(
+    <AppContainer>
+        <UserProvider>
+            <App />
+        </UserProvider>
+    </AppContainer>,
+    root
+)
+// See last lines
+serviceWorker.register()
+// function renderToDOM() {
+//     const root = document.getElementById('root')
+//
+//     if (root !== null) {
+//         ReactDOM.render(
+//             <UserProvider>
+//                 <App />
+//             </UserProvider>,
+//             root
+//         )
+//     }
+// }
+
+// export { renderToDOM }
+
+// renderToDOM()
+
+if ((module as any).hot) {
+    (module as any).hot.accept('./App', () => {
+        const NextApp = require('./App').default;
         ReactDOM.render(
-            <UserProvider>
-                <App />
-            </UserProvider>,
+            <AppContainer>
+                <UserProvider>
+                    <NextApp />
+                </UserProvider>
+            </AppContainer>,
             root
         )
-    }
+    })
 }
-
-export { renderToDOM }
-
-renderToDOM()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// serviceWorker.unregister()
