54f437a6e654a72dbbcedfcb48c83f9d00c8fdc5
alexcos20 <alex.coseru@gmail.com>
2020-05-18 10:02:40 -0700

    fix tests

--- client/src/utils/getUserJobs.ts
+++ client/src/utils/getUserJobs.ts
@@ -2,8 +2,6 @@ import { Account } from '@oceanprotocol/squid'
 
 export async function getUserJobs(ocean: any, account: string) {
     try {
-        let account: Account
-        ;[account] = await ocean.accounts.list()
         const account = await ocean.accounts.list()
 
         await account.authenticate()
