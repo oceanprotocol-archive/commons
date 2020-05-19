8831c46e397221542d30e6c594be8844b4869012
alexcos20 <alex.coseru@gmail.com>
2020-05-19 09:23:38 -0700

    fix history

--- client/src/utils/getUserJobs.ts
+++ client/src/utils/getUserJobs.ts
@@ -2,10 +2,11 @@ import { Account } from '@oceanprotocol/squid'
 
 export async function getUserJobs(ocean: any, account: string) {
     try {
-        const account = await ocean.accounts.list()
+        const accounts = await ocean.accounts.list()
+        const account=accounts[0]
 
         await account.authenticate()
-        const jobList = await ocean.compute.status(account[0])
+        const jobList = await ocean.compute.status(account)
         return jobList
     } catch (error) {
         console.error(error)
